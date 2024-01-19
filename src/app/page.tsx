8a336b2853f6de8b2053091cb4043987139dc442
Paulo Calixto <Paulo_Calixto@gap.com>
2024-01-19 17:07:47 -0300

    [rabbit-4]: Initial next.js project with Hello World

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.h1}>Hello World</h1>
+    </main>
+  );
+}
