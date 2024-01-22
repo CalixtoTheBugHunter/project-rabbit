13ce506d937d4583a4bcf9827d854630779cb227
Paulo Calixto <Paulo_Calixto@gap.com>
2024-01-22 13:16:44 -0300

    test: jest and jest-axe integration

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,7 @@
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+      <h1 className={styles.h1}>Hello World</h1>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Header } from "./components";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.h1}>Hello World</h1>
+      <Header />
     </main>
   );
 }
